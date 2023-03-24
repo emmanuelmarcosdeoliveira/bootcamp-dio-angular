a4b143d9665fd15e482bb0bba0af9e53a4f3a1e1
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-03-24 08:56:20 -0300

    aula-01

---
+++ javascript/aula.js
@@ -0,0 +1,6 @@
+let variavel = 10 / 10;
+//variavel pode ser alterado o valor;
+// usando o operador de soma. 
+const pi = 3.14;
+// const é um constante e não podemos mudar o valor.
+console.log(variavel);
