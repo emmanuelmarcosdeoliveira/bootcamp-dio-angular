9ab6ca53239f80dc466408790d6d0a7492483413
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-03-24 22:55:24 -0300

    aula-02

---
+++ javascript/aula-02/aula02.js
@@ -0,0 +1,19 @@
+// condicionais 
+
+// const numero  = 0;
+// const numeroPar  = numero % 2 === 0 ;
+// if (numeroPar) {
+//   console.log("Par")
+
+// }else {
+//   console.log("Impar")
+//   }
+
+const numero  = 5;
+const isnumeroPar = (numero % 2) === 0 ;
+
+if(isnumeroPar){
+console.log("O numnero é par"); 
+}else {
+  console.log("O numero é Impar")
+  }
---
+++ javascript/aula-02/segundo_desafio.js
@@ -0,0 +1,11 @@
+// Faça um programa para calular o valor de uma viagem
+
+// Você tera 05 variáveis sendo elas 
+//   1 - Preço do Etanol
+//   2 - Preço da Gasolina
+//   3 - O tipo de combustivel que está no seu carro. 
+//   4 - Gasto médio de combustível por KM 
+//   5 - Distância em KM da Viagem 
+
+// Imprima no console o valor que será gasto para realizar esta viagem.
+
